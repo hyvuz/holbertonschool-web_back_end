ff46b25099b2e6dff6afbc5f72f58704b40d23b4
hyvuz <haneennasser1425@outlook.com>
2025-07-31 18:10:00 +0300

    Add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a function that returns the floor of a float without using math module."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n and handle negative cases."""
+    i = int(n)
+    if n < 0 and n != i:
+        return i - 1
+    return i
