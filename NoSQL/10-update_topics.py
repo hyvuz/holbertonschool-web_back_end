04b09f4faabddb6c73684d630c562e5770f8a093
hyvuz <haneennasser1425@outlook.com>
2025-08-17 23:41:51 +0300

    Add 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Update topics list for all school documents matching a given name."""
+from typing import List
+
+
+def update_topics(mongo_collection, name: str, topics: List
